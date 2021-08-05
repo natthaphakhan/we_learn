c7fa4c7826aa4490eef56be11a1d51e9eb24a1e0
ANWA <natthaphakhan@gmail.com>
2021-08-05 14:10:46 +0700

    1234

--- lib/page/noti/noti.dart
+++ lib/page/noti/noti.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class Noti extends StatefulWidget {
   const Noti({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class Noti extends StatefulWidget {
 }
 
 class _NotiState extends State<Noti> {
+  bool tapEdit = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -30,7 +32,7 @@ class _NotiState extends State<Noti> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 80),
+                  padding: const EdgeInsets.only(left: 100),
                   child: Text(
                     'แจ้งเตือน',
                     style: TextStyle(
@@ -45,19 +47,168 @@ class _NotiState extends State<Noti> {
           actions: [
             Padding(
               padding: const EdgeInsets.only(right: 28),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Noti()),
-                  );
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (tapEdit == false) {
+                      tapEdit = true;
+                    } else {
+                      tapEdit = false;
+                    }
+                  });
                 },
-                child: Icon(
-                  Icons.notifications_outlined,
-                  color: Color.fromRGBO(111, 116, 183, 1),
-                ),
+                child: tapEdit == false
+                    ? Text(
+                        'แก้ไข',
+                        style: TextStyle(color: Colors.black),
+                      )
+                    : Text(
+                        'เลือกทั้งหมด',
+                        style: TextStyle(color: Colors.black),
+                      ),
               ),
             )
+          ],
+        ),
+        body: Column(
+          children: [
+            Slidable(
+              actionPane: SlidableBehindActionPane(),
+              secondaryActions: [
+                Image.asset(
+                  'assets/delete_noti.png',
+                  fit: BoxFit.cover,
+                )
+              ],
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey[300]!)),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.message_outlined,
+                        color: Color.fromRGBO(111, 116, 183, 1),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            'การแจ้งเตือนจากระบบ 12/07/2564 10:35 น.ระบบจะทำการปรับปรุงแก้ไขและปิดระบบชั่วคราวในวันที่ 19/07/2564 เวลา 02.00 - 05.00 น. ขออภัยในความไม่สะดวก'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Slidable(
+              actionPane: SlidableBehindActionPane(),
+              secondaryActions: [
+                Image.asset(
+                  'assets/delete_noti.png',
+                  fit: BoxFit.cover,
+                )
+              ],
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey[300]!)),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.message_outlined,
+                        color: Color.fromRGBO(111, 116, 183, 1),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            'การแจ้งเตือนจากระบบ 12/07/2564 10:35 น.ระบบจะทำการปรับปรุงแก้ไขและปิดระบบชั่วคราวในวันที่ 19/07/2564 เวลา 02.00 - 05.00 น. ขออภัยในความไม่สะดวก'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Slidable(
+              actionPane: SlidableBehindActionPane(),
+              secondaryActions: [
+                Image.asset(
+                  'assets/delete_noti.png',
+                  fit: BoxFit.cover,
+                )
+              ],
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey[300]!)),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.message_outlined,
+                        color: Color.fromRGBO(111, 116, 183, 1),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            'การแจ้งเตือนจากระบบ 12/07/2564 10:35 น.ระบบจะทำการปรับปรุงแก้ไขและปิดระบบชั่วคราวในวันที่ 19/07/2564 เวลา 02.00 - 05.00 น. ขออภัยในความไม่สะดวก'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: SizedBox(),
+            ),
+            tapEdit == true
+                ? Container(
+                    width: double.maxFinite,
+                    height: 50,
+                    color: Color.fromRGBO(111, 116, 183, 1),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'อ่านแล้ว',
+                              style: TextStyle(
+                                  color: Colors.yellow[700],
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Text('ลบ',
+                                style: TextStyle(
+                                  color: Colors.yellow[700],
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(
+                    child: null,
+                  )
           ],
         ),
       ),
